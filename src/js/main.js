529b181625aad3f60687d592b297777f1fefc3e7
FizzahZa <szahoor90@gmail.com>
2019-04-28 00:21:08 +0530

    Add Project Code Base - FE

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,56 @@
-(function global() {
-  // console.log('JS');
-})();
+$(document).ready(function() {
+  jQuery('.slider').slick({
+    infinite: false,
+    dots: true,
+    arrows: false,
+    speed: 300,
+    mobileFirst: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.cross').hide();
+  $('.hamburger').click(function() {
+    $('.header_navbar').slideToggle('slow', function() {
+      $('.hamburger').hide();
+      $('.cross').show();
+    });
+  });
+
+  $('.cross').click(function() {
+    $('.header_navbar').slideToggle('slow', function() {
+      $('.cross').hide();
+      $('.hamburger').show();
+    });
+  });
+});
